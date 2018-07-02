a780814a760469556768a789e2b818a9206a4d78
Uche Jude <iamuchejude@gmail.com>
2018-07-02 15:31:12 +0100

    Completed Signup Page

--- UI/js/main.js
+++ UI/js/main.js
@@ -24,13 +24,42 @@ if (registerForm !== null || undefined) {
             // Display Error
             showMessage('All fields must not be empty', 'danger');
         } else {
+            let url = 'https://more-recipes.herokuapp.com/api/v1/users/signup';
             let data = {
-                'fullname': fullName.value,
-                'email': email.value,
-                'password': email.value,
+                "user": {
+                    'fullname': fullName.value,
+                    'email': email.value,
+                    'password': email.value,
+                }
             }
-            // Display Success
-            showMessage('Proceed with registration', 'success');
+
+            var headers = new Headers();
+            headers.append('Content-Type', 'application/json');
+
+            var request = new Request(url, {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers: headers
+            });
+
+            fetch(request)
+            .then((res) => res.json())
+            .then((data) => {
+                if (data.status === 'error') {
+                    showMessage(`Error Occured! ${data.message}`, 'error');
+                } else {
+                    showMessage('Registration was successful. You will be redirected in 10 seconds', 'success');
+                    localStorage.setItem('token', data.token);
+                    localStorage.setItem('isLogin', true);
+
+                    window.setTimeout(() => {
+                        window.location = '/UI/dashboard.html';
+                    }, 5000);
+                }
+            })
+            .catch((err) => {
+                if (err) showMessage('Unexpected Error Occured! Please try again later', 'error');
+            })
         }
     });
 }
